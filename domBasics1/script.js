d4ee4d004c41c699c752921b10459056e1d0c5ba
Bradley <bradleydoyle24@gmail.com>
2023-06-03 16:51:10 -0400

    Modify 'script.js' to complete exercise
    Used javascript to create new div, <p>, and <h1> with different
    color texts and backgrounds.

--- domBasics1/script.js
+++ domBasics1/script.js
@@ -4,4 +4,23 @@ const content = document.createElement('div');
 content.classList.add('content');
 content.textContent = 'This is the glorious text content!';
 
+const redText = document.createElement('p');
+redText.style.cssText = 'color: red';
+redText.textContent = 'Hey I\'m red!'
+content.appendChild(redText);
 container.appendChild(content);
+
+const pinkDiv = document.createElement('div');
+  pinkDiv.style.cssText = 'border-style: solid; border-thickness: 1em; border-color: black; background-color: pink';
+
+
+const header1 = document.createElement('h1');
+  header1.textContent = 'I\'m in a div';
+  pinkDiv.appendChild(header1);
+
+const meToo = document.createElement('div');
+  meToo.textContent = 'ME TOO!';
+  pinkDiv.appendChild(meToo);
+
+
+content.appendChild(pinkDiv);
